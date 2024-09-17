30f6b331891a842ec2f5b26adc9d905ab7edaa9f
Vadim Dremlyuga <vdremlyuga@gmail.com>
2024-09-17 23:29:19 +0300

    1.3

--- alg6.py
+++ alg6.py
@@ -4,19 +4,16 @@ def block_sort(array_size: int, array: list) -> int:
 
     while True:
         switch = False
-        # start_index = array_index
-        # while (switch is False or array_index < max_number + 1 - start_index) and array_index < len(array):
-        while (switch is False or array_index < max_number + 1) and array_index < len(array):
+        while switch is False or array_index < max_number + 1:
             if array[array_index] == target:
                 switch = True
             if array[array_index] > max_number:
                 max_number = array[array_index]
             array_index += 1
+            if array_index == len(array):
+                return count +1
         count += 1
         target = max_number + 1
-        if array_index == len(array):
-            break
-    return count
 
 
 if __name__ == '__main__':
