1b83cb8237825d689cdeb1502247a3c4e8c19ee4
vsdremlyuga <vsdremlyuga@cloud.ru>
2024-09-20 11:54:10 +0300

    1.7

--- string_decrypt.py
+++ string_decrypt.py
@@ -3,11 +3,17 @@ import string
 
 
 def string_decrypt(encrypted_string: str) -> str:
+    '''Дешифрование управляющих сообщений Марсохода.
+    Входные данные: encrypted_string - шифровонное сообщение
+    Выходные данные: decrypted_string - дешифрованное сообщение
+    '''
+    #  Если шифрованная строка пустая вернуть также пустую строку
     if encrypted_string == '':
         return ''
     index = 0
     decrypted_string = ''
     while index < len(encrypted_string):
+        # Получение числа-множителя из строки
         if encrypted_string[index] in string.digits:
             index_left = index
             while encrypted_string[index + 1] in string.digits:
@@ -16,7 +22,7 @@ def string_decrypt(encrypted_string: str) -> str:
             index += 1
         else:
             multiplier = 1
-
+        # Поиск и дешифрование подстрок
         if encrypted_string[index] == '[':
             start_index = index + 1
             count = 1
