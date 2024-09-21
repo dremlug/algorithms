252d75bf3aafd9edf7e92b713f9b70d053eb9725
Vadim Dremlyuga <vdremlyuga@gmail.com>
2024-09-21 22:06:38 +0300

    2.0

--- string_decrypt.py
+++ string_decrypt.py
@@ -1,36 +1,32 @@
-# ID 118483749
+# ID 118490491
 import string
 
 
 def decrypt(encrypted: str) -> str:
     """Дешифрование управляющих сообщений Марсохода.
     Входные данные: encrypted - шифровонное сообщение
-    Выходные данные: current_decryption - дешифрованное сообщение
+    Выходные данные: decrypted - дешифрованное сообщение
     """
-    multiplier = ''
-    decrypted = []
-    current_decrypted = ''
+    multiplier = decrypted = ''
+    _stack = []
     for char in encrypted:
         # Получение числа-множителя из строки
         if char in string.digits:
-                multiplier += char
-                continue
+            multiplier += char
         # Добавляем подстроку и множитель в стек
         elif char == '[':
-            decrypted.append((multiplier, current_decrypted))
-            current_decrypted = ''
+            _stack.append((int(multiplier), decrypted))
+            decrypted = ''
+            multiplier = ''
         # Вынимаем подстроку и множитель из стека
         elif char == ']':
-            _multiplier, _string = decrypted.pop()
-            current_decrypted = _string + int(_multiplier) * current_decrypted
+            _multiplier, _decrypted = _stack.pop()
+            decrypted = _decrypted + _multiplier * decrypted
         # Добавлем символ с коэффициентом в подстроку
         else:
-            if multiplier != '':
-                current_decrypted += int(multiplier) * char
-            else:
-                current_decrypted += char
-        multiplier = ''
-    return current_decrypted
+            decrypted += char
+
+    return decrypted
 
 
 if __name__ == '__main__':
